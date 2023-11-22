abf90bf688b0510ec6c22f11c7a203baefc60505
IDRERD <idrerd999@gmail.com>
2023-11-22 20:30:35 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+import random
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.drawCircle)
+        self.r = None
+
+    def drawCircle(self):
+        self.r = random.randint(10, 200)
+        self.repaint()
+
+    def paintEvent(self, qp):
+        if self.r:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(255, 255, 0))
+            qp.drawEllipse(int(self.width() / 2 - self.r), int(self.height() / 2 - self.r / 2), self.r * 2, self.r * 2)
+            qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec_())
